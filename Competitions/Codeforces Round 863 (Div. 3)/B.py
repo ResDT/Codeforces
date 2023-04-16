91a5eac86e4211a26aca23eefcd4641ae96b762d
ResDT <shiryaev.nikita.resdt@gmail.com>
2023-04-16 15:36:38 +0300

    Solved

--- Competitions/Codeforces Round 863 (Div. 3)/B.py
+++ Competitions/Codeforces Round 863 (Div. 3)/B.py
@@ -1,16 +1,18 @@
-def number_of_square(n, x, y):
-    for number in range(n // 2, 0, -1):
-        if number <= x <= n - number + 1 and number <= y <= n - number + 1:
-            return number
-
-
 def main():
     t = int(input())
 
     for step in range(t):
         n, x_1, y_1, x_2, y_2 = map(int, input().split())
+        check_1 = (
+            min(x_1, n - y_1 + 1) if x_1 < y_1
+            else min(y_1, n - x_1 + 1)
+        )
+        check_2 = (
+            min(x_2, n - y_2 + 1) if x_2 < y_2
+            else min(y_2, n - x_2 + 1)
+        )
 
-        print(abs(number_of_square(n, x_1, y_1) - number_of_square(n, x_2, y_2)))
+        print(abs(check_1 - check_2))
 
 
 main()
